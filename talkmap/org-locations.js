d89ec94908f9477315db3cef41ce733d5db43d67
mverdicchio <verdicchio@gmail.com>
2025-05-19 12:30:20 -0400

    added talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,62 @@
 var addressPoints = [
   [
-    "Tutorial 1 on Relevant Topic in Your Field<br />UC-Berkeley Institute for Testing Science; Berkeley, CA, USA",
-    37.8708393,
-    -122.272863
+    "Adapting program assessment for the age of generative AI<br />IEEE Engineering Education World Conference (EDUNINE); Virtual",
+    44.4939936,
+    11.3576917
   ],
   [
-    "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
-    37.7792588,
-    -122.4193286
+    "Embedding cybersecurity in the second programming course (CS2)<br />Consortium for Computing Sciences in Colleges Southeastern Conference; Furman University",
+    34.9257743,
+    -82.4385258
   ],
   [
-    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
-    34.0536909,
-    -118.242766
+    "Effective pedagogical practices in the computer science classroom<br />Consortium for Computing Sciences in Colleges Southeastern Conference; Greenville, SC",
+    34.851354,
+    -82.3984882
   ],
   [
-    "Talk 2 on Relevant Topic in Your Field<br />London School of Testing; London, UK",
-    51.5074456,
-    -0.1277653
+    "Identifying targets for intervention by analyzing basins of attraction<br />Pacific Symposium on Biocomputing; Waimea, HI",
+    20.020452,
+    -155.66391
+  ],
+  [
+    "Boolean Network Models of Human Aging<br />7th Annual Rocky Mountains Bioinformatics Conference; Snowmass, CO",
+    39.3310391,
+    -106.9846471
+  ],
+  [
+    "The Impact of AI Tools on Assessment Data<br />ABET Symposium; Tampa, FL",
+    27.9477595,
+    -82.458444
+  ],
+  [
+    "Creating a DevOps course<br />ACM Technical Symposium on Computer Science Education (SIGCSE); Toronto, Canada",
+    43.6534817,
+    -79.3839347
+  ],
+  [
+    "Reverse Engineering and Synthesis of Biomolecular Systems<br />Pacific Symposium on Biocomputing; Waimea, HI",
+    20.020452,
+    -155.66391
+  ],
+  [
+    "Learning Causal Relationships between Genes from Steady State Data: Algorithms, Simulation and Application<br />6th Annual Rocky Mountains Bioinformatics Conference; Snowmass, CO",
+    39.3310391,
+    -106.9846471
+  ],
+  [
+    "Hurricanes and pandemics: An experience report on adapting software engineering courses to ensure continuity of instruction<br />Consortium for Computing Sciences in Colleges Southeastern Conference; Virtual",
+    44.4939936,
+    11.3576917
+  ],
+  [
+    "An Interactive Environment for Visualizing, Interpreting, and Revising Biological Process Models<br />Research in Computational Molecular Biology (RECOMB); Tucson, AZ",
+    32.2228765,
+    -110.974847
+  ],
+  [
+    "Template-Based Intervention in Boolean Network Models of Biological Systems<br />International Conference on Intelligent Systems for Molecular Biology (ISMB); Boston, MA",
+    42.3554334,
+    -71.060511
   ]
 ];
